5a0ff29833cc9d7c7f876d5bfccc616a9b5daa75
Saksham gupta <guptasaksham3071gmail.com>
2024-03-11 11:32:43 +0530

    Refresh Token Added

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 const express = require('express');
 const axios = require('axios');
+const { setIntervalAsync } = require('set-interval-async/dynamic');
 
 const app = express();
 app.use(express.json());
 
-let ZOHO_CRM_ACCESS_TOKEN = '1000.0fad0bece2b4528bdb2b520493a08a74.4802d929d1b366c9532867a3abb521fb'; // This will be updated dynamically
+let ZOHO_CRM_ACCESS_TOKEN = '';
 
-// Function to post a new lead to Zoho CRM API, correctly handling JSON data
+async function fetchAccessToken() {
+    const accessTokenUrl = "https://script.googleusercontent.com/macros/echo?user_content_key=4ZS3JWehoZe9AG4UeNXy5t0frgaKWFkRDX2I42pBCSTNWS170H-goZvaHYSjVbxFsM4WEmw5GNb6O-MQ5yhIVRRwK8wQslAWm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnP-u-p9iTG4jdgmUvWl3RYGEI1cvNowZuDl8EcfID0pm0Kc1gdImgzUha5MgDMQrEwZvnmwH8tzlu022UqIZfQsauDmOpfYrHNz9Jw9Md8uu&lib=MPo20bEL0RwzBzCUXHRmQVrtrsWyMfJRS";
+
+    try {
+        const response = await axios.get(accessTokenUrl);
+        return response.data.trim();
+    } catch (error) {
+        console.error("Error fetching access token:", error);
+        throw error;
+    }
+}
+
+async function updateAccessToken() {
+    try {
+        const accessToken = await fetchAccessToken();
+        ZOHO_CRM_ACCESS_TOKEN = accessToken;
+        console.log("Zoho CRM access token updated:", ZOHO_CRM_ACCESS_TOKEN);
+    } catch (error) {
+        console.error("Error updating access token:", error);
+    }
+}
+
+updateAccessToken();
+
+const accessTokenUpdateInterval = 60 * 60 * 1000;
+setIntervalAsync(updateAccessToken, accessTokenUpdateInterval);
+
 async function postLead(leadData) {
     const config = {
         method: 'post',
         url: 'https://www.zohoapis.in/crm/v2/Leads',
         headers: {
             'Authorization': `Zoho-oauthtoken ${ZOHO_CRM_ACCESS_TOKEN}`,
-            'Content-Type': 'application/json' // Ensure correct content type for JSON
+            'Content-Type': 'application/json'
         },
-        data: JSON.stringify(leadData) // Convert lead data to JSON string
+        data: JSON.stringify(leadData)
     };
 
     try {
         return await axios(config);
     } catch (error) {
         console.error('Error in postLead function:', error);
-        throw error; // Rethrowing the error for handling in the calling function
+        throw error;
     }
 }
 
@@ -49,10 +76,9 @@ async function postLeadToZohoCRM(lead) {
     }
 }
 
-// Endpoint to receive webhook requests from Kylas
 app.post('/kylas-webhook', async (req, res) => {
     try {
-        const newLead = req.body; // Assuming req.body is directly the lead object
+        const newLead = req.body;
         await postLeadToZohoCRM(newLead);
         res.status(200).send('Lead processed successfully');
     } catch (error) {
