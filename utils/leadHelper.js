4ce00f7f6bb088d42de92ab5848f65e41d34c5ee
Saksham gupta <guptasaksham3071gmail.com>
2024-03-12 15:29:56 +0530

    Update lead

--- utils/leadHelper.js
+++ utils/leadHelper.js
@@ -28,8 +28,8 @@ exports.postLeadToZohoCRM = async (lead) => {
         const leadData = {
             data: [
                 {
-                    First_Name: lead.entity.firstName,
-                    Last_Name: lead.entity.lastName,
+                    First_Name: lead.entity.firstName || "",
+                    Last_Name: lead.entity.lastName || "",
                     Phone: (lead.entity.phoneNumbers[0].dialCode + lead.entity.phoneNumbers[0].value) || "",
                     Email: (lead.entity.emails == null) ? "" : (lead.entity.emails[0].value),
                     City: lead.entity.city || "",
@@ -49,10 +49,10 @@ exports.postLeadToZohoCRM = async (lead) => {
 }
 
 // Update Leads
-const updateLead = async (leadData) => {
+const updateLead = async (leadData, leadId) => {
     const config = {
-        method: 'post',
-        url: 'https://www.zohoapis.in/crm/v2/Leads',
+        method: 'put',
+        url: 'https://www.zohoapis.in/crm/v2/Leads/${leadId}',
         headers: {
             'Authorization': `Zoho-oauthtoken ${ZOHO_CRM_ACCESS_TOKEN}`,
             'Content-Type': 'application/json'
@@ -92,33 +92,30 @@ exports.updateLeadToZohoCRM = async (lead) => {
     let phoneData = lead.entity.phoneNumbers[0].value;
     console.log("update lead data");
     console.log(lead);
-    console.log(phoneData);
     const leadId = await getLeadIdByPhoneNumber(phoneData);
-    console.log("leadId");
-    console.log(leadId);
 
 
-    // try {
-    //     const leadData = {
-    //         data: [
-    //             {
-    //                 First_Name: lead.entity.firstName,
-    //                 Last_Name: lead.entity.lastName,
-    //                 Phone: (lead.entity.phoneNumbers[0].dialCode + lead.entity.phoneNumbers[0].value) || "",
-    //                 Email: (lead.entity.emails == null) ? "" : (lead.entity.emails[0].value),
-    //                 City: lead.entity.city || "",
-    //                 State: lead.entity.state || "",
-    //                 Zip_Code: lead.entity.zipcode || "",
-    //                 Lead_Source: lead.entity.source.value || "",
-    //                 Kylas_Owner: lead.entity.ownerId.value || "",
-    //             },
-    //         ],
-    //     };
+    try {
+        const leadData = {
+            data: [
+                {
+                    First_Name: lead.entity.firstName,
+                    Last_Name: lead.entity.lastName,
+                    Phone: (lead.entity.phoneNumbers[0].dialCode + lead.entity.phoneNumbers[0].value) || "",
+                    Email: (lead.entity.emails == null) ? "" : (lead.entity.emails[0].value),
+                    City: lead.entity.city || "",
+                    State: lead.entity.state || "",
+                    Zip_Code: lead.entity.zipcode || "",
+                    Lead_Source: lead.entity.source.value || "",
+                    Kylas_Owner: lead.entity.ownerId.value || "",
+                },
+            ],
+        };
 
-    //     const response = await updateLead(leadData);
-    //     console.log('Lead posted to Zoho CRM successfully:', response.data);
-    // } catch (error) {
-    //     console.error('Error posting lead to Zoho CRM:', error.response ? error.response.data : error);
-    // }
+        const response = await updateLead(leadData, leadId);
+        console.log('Lead posted to Zoho CRM successfully:', response.data);
+    } catch (error) {
+        console.error('Error posting lead to Zoho CRM:', error.response ? error.response.data : error);
+    }
 }
 
