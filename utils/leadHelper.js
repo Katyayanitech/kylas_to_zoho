49109b49cd99988fd2d6a242aed731f323737818
Katyayani tech2 <guptasaksham3071@gmail.com>
2024-03-20 11:05:59 +0530

    Lead status update

--- utils/leadHelper.js
+++ utils/leadHelper.js
@@ -37,6 +37,7 @@ exports.postLeadToZohoCRM = async (lead) => {
                     Zip_Code: lead.entity.zipcode || "",
                     Lead_Source: lead.entity.source.value || "",
                     Kylas_Owner: lead.entity.ownerId.value || "",
+                    Lead_Status: lead.entity.status.name || ""
                 },
             ],
         };
@@ -121,6 +122,7 @@ exports.updateLeadToZohoCRM = async (lead) => {
                         Zip_Code: lead.entity.zipcode || "",
                         Lead_Source: lead.entity.source.value || "",
                         Kylas_Owner: lead.entity.ownerId.value || "",
+                        Lead_Status: lead.entity.status.name || ""
                     },
                 ],
             };
