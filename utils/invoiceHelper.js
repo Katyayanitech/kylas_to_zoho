7856a274dad44fef4a8ba42574c8c365cf5e5091
Katyayani tech2 <guptasaksham3071@gmail.com>
2024-03-26 12:23:04 +0530

    invoice commit 1

--- utils/invoiceHelper.js
+++ utils/invoiceHelper.js
@@ -1,6 +1,25 @@
 const axios = require('axios');
 
-const Postinvoice = async (invoiceData) => {
+const searchProductBySKU = async (sku) => {
+    const config = {
+        method: 'get',
+        url: `https://www.zohoapis.in/crm/v2/Products/search?criteria=(Product_Code:equals:${encodeURIComponent(sku)})`,
+        headers: {
+            'Authorization': `Zoho-oauthtoken ${ZOHO_CRM_ACCESS_TOKEN}`,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const response = await axios(config);
+        return response.data.data[0];
+    } catch (error) {
+        console.log('Error searching product by SKU:', error);
+        throw error;
+    }
+};
+
+const postInvoice = async (invoiceData) => {
     const config = {
         method: 'post',
         url: 'https://www.zohoapis.in/crm/v2/Invoices',
@@ -14,10 +33,10 @@ const Postinvoice = async (invoiceData) => {
     try {
         return await axios(config);
     } catch (error) {
-        console.log('Error in postContact function:', error);
+        console.log('Error posting invoice to Zoho CRM:', error);
         throw error;
     }
-}
+};
 
 exports.PostBookToCRM = async (invoice) => {
     console.log("invoice");
@@ -25,63 +44,67 @@ exports.PostBookToCRM = async (invoice) => {
     console.log("Line Items");
     console.log(invoice.invoice.line_items);
 
-    // try {
-    //     const invoiceData = {
-    //         data: [
-    //             {
-    //                 "Owner": {
-    //                     "name": "Katyayani Manager",
-    //                     "id": "431127000000257001",
-    //                     "email": "katyayanimanager@gmail.com"
-    //                 },
-    //                 "Payment_Type": invoice.invoice.custom_field_hash.cf_terms_of_payment,
-    //                 "Currency": invoice.invoice.currency_code,
-    //                 "Invoice_Date": invoice.invoice.date,
-    //                 "Grand_Total": invoice.invoice.total,
-    //                 "Contact_Name": {
-    //                     "name": "Katyayani organics",
-    //                     "id": "431127000007006876"
-    //                 },
-    //                 "Account_Name": {
-    //                     "name": "Katyayani Organics",
-    //                     "id": "431127000001985473"
-    //                 },
-    //                 "Status": invoice.invoice.status,
-    //                 "Shipping_State": invoice.invoice.shipping_address.state,
-    //                 "Subject": invoice.invoice.invoice_number,
-    //                 "Product_Details": [
-    //                     {
-    //                         "product": {
-    //                             "Product_Code": null,
-    //                             "Currency": "INR",
-    //                             "name": "Katyayani Activated Humic Acid + Fulvic Acid 98800 GM (800gm x 1)",
-    //                             "id": "431127000004118252"
-    //                         },
-    //                         "quantity": 1,
-    //                         "Discount": 0,
-    //                         "total_after_discount": 1,
-    //                         "net_total": 1,
-    //                         "book": null,
-    //                         "Tax": 0,
-    //                         "list_price": 1,
-    //                         "unit_price": 1,
-    //                         "quantity_in_stock": -1,
-    //                         "total": 1,
-    //                         "id": "431127000013198002",
-    //                         "product_description": null,
-    //                         "line_tax": []
-    //                     }
-    //                 ]
-    //             },
-    //         ],
-    //     };
+    try {
+        const productDetails = [];
 
-    //     console.log("invoiceData");
-    //     console.log(invoiceData);
+        for (let i = 0; i < invoice.invoice.line_items.length; i++) {
+            const lineItem = invoice.invoice.line_items[i];
+            const product = await searchProductBySKU(lineItem.sku);
 
-    //     const response = await Postinvoice(invoiceData);
-    //     console.log('Invoice posted to Zoho CRM successfully:', response.data);
-    // } catch (error) {
-    //     console.log('Error posting invoice to Zoho CRM:', error.response ? error.response.data : error);
-    // }
-}
+            const productDetail = {
+                "product": {
+                    "id": product.id
+                },
+                "quantity": lineItem.quantity,
+                "Discount": lineItem.discount,
+                "total_after_discount": lineItem.total - lineItem.discount,
+                "net_total": lineItem.total,
+                "Tax": lineItem.tax_total,
+                "list_price": lineItem.rate,
+                "unit_price": lineItem.rate,
+                "quantity_in_stock": lineItem.quantity,
+                "total": lineItem.total,
+                "product_description": lineItem.description || null,
+                "line_tax": []
+            };
+
+            productDetails.push(productDetail);
+        }
+
+        const invoiceData = {
+            data: [
+                {
+                    "Owner": {
+                        "name": "Katyayani Manager",
+                        "id": "431127000000257001",
+                        "email": "katyayanimanager@gmail.com"
+                    },
+                    "Payment_Type": invoice.invoice.custom_field_hash.cf_terms_of_payment,
+                    "Currency": invoice.invoice.currency_code,
+                    "Invoice_Date": invoice.invoice.date,
+                    "Grand_Total": invoice.invoice.total,
+                    "Contact_Name": {
+                        "name": "Katyayani organics",
+                        "id": "431127000007006876"
+                    },
+                    "Account_Name": {
+                        "name": "Katyayani Organics",
+                        "id": "431127000001985473"
+                    },
+                    "Status": invoice.invoice.status,
+                    "Shipping_State": invoice.invoice.shipping_address.state,
+                    "Subject": invoice.invoice.invoice_number,
+                    "Product_Details": productDetails
+                },
+            ],
+        };
+
+        console.log("invoiceData");
+        console.log(invoiceData);
+
+        const response = await postInvoice(invoiceData);
+        console.log('Invoice posted to Zoho CRM successfully:', response.data);
+    } catch (error) {
+        console.log('Error posting invoice to Zoho CRM:', error.response ? error.response.data : error);
+    }
+};
