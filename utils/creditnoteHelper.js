d1592b8f4fcdf52a611189f39b473f6e0ff9c21c
Katyayani tech2 <guptasaksham3071@gmail.com>
2024-04-01 17:09:53 +0530

    Credit Note 13

--- utils/creditnoteHelper.js
+++ utils/creditnoteHelper.js
@@ -34,14 +34,14 @@ const invoiceSearchById = async (id) => {
     try {
         const response = await axios(config);
         console.log(response.data);
-        // const invoice = response.data.data[0];
-        // if (!invoice) {
-        //     throw new Error('Invoice not found');
-        // }
-        // const invoiceId = invoice["id"];
-        // console.log(invoiceId);
+        const invoice = response.data.data[0];
+        if (!invoice) {
+            throw new Error('Invoice not found');
+        }
+        const invoiceId = invoice["id"];
+        console.log(invoiceId);
 
-        // return { success: true, id: invoiceId };
+        return { success: true, id: invoiceId };
 
     } catch (error) {
         console.error('Error searching invoice by ID:', error.message);
