89b898d83ed83852a0553844b36795b2b35d0463
katyayanitech2 <katyayanitech2@gmail.com>
2024-04-05 18:05:14 +0530

    chat update last

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ const contact = require("./routes/contact.js");
 const deal = require("./routes/deal.js");
 const task = require("./routes/task.js");
 const call = require("./routes/call.js");
-// const invoice = require("./routes/invoice.js");
 const creditnote = require("./routes/creditnote.js");
-const { scheduleAPIPolling } = require("./utils/indexHelpers/helper.js");
-const { ZohoBookToCRMInvoice } = require("./utils/invoiceHelper.js");
-const { ZohoCRMToKylasChatLeads } = require("./utils/chatLeadsHelper.js");
 
 require("dotenv").config();
 const app = express();
@@ -22,8 +18,6 @@ app.use("/deal", deal);
 app.use("/task", task);
 app.use("/call", call);
 
-// Books To CRM 
-// app.use("/invoice", invoice);
 app.use("/creditnote", creditnote);
 
 
@@ -33,10 +27,6 @@ updateAccessToken();
 
 const accessTokenUpdateInterval = 10 * 60 * 1000;
 setIntervalAsync(updateAccessToken, accessTokenUpdateInterval);
-
-scheduleAPIPolling();
-ZohoBookToCRMInvoice();
-ZohoCRMToKylasChatLeads();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- routes/invoice.js
+++
@@ -1,8 +0,0 @@
-// const express = require("express");
-// const { postBookInvoiceToCRM } = require("../controller/invoice");
-
-// const router = express.Router();
-
-// router.post('/zohobook-invoice', postBookInvoiceToCRM);
-
-// module.exports = router;
