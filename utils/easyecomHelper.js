cbfcdbf59563fa9222001f753866f3b6535e55f1
katyayanitech2 <katyayanitech2@gmail.com>
2024-04-19 13:49:23 +0530

    invoice7

--- utils/easyecomHelper.js
+++ utils/easyecomHelper.js
@@ -38,20 +38,21 @@ const getCustomerId = async (phoneNumber) => {
 const postInvoiceToBooks = async (easycomData) => {
     const ZOHO_BOOK_ACCESS_TOKEN = await generateAuthToken();
     console.log("ZohoBookToken", ZOHO_BOOK_ACCESS_TOKEN);
+
     const config = {
         method: 'post',
         url: 'https://www.zohoapis.in/books/v3/invoices?organization_id=60019077540',
         headers: {
             'Authorization': `Zoho-oauthtoken ${ZOHO_BOOK_ACCESS_TOKEN}`,
-            'content-type': 'application/json'
+            'Content-Type': 'application/json'
         },
-        data: JSON.stringify(easycomData)
+        data: easycomData
     };
 
     try {
         return await axios(config);
     } catch (error) {
-        console.log('Error in postLead function:', error);
+        console.log('Error in postInvoiceToBooks function:', error);
         // throw error;
     }
 }
