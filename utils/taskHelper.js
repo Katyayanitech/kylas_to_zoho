7dcb2116b7e383ea3d9766cb694af8a85934781a
praveen soni <120718170+CodewithPKs@users.noreply.github.com>
2024-06-18 14:10:37 +0530

    status log

--- utils/taskHelper.js
+++ utils/taskHelper.js
@@ -208,7 +208,7 @@ exports.updateTaskToZohoCRM = async (task) => {
     console.log(`System : ${systemApproved}`);
     const dueDate = new Date(task.entity.dueDate);
     const formattedDueDate = `${dueDate.getFullYear()}-${(dueDate.getMonth() + 1).toString().padStart(2, '0')}-${dueDate.getDate().toString().padStart(2, '0')}`;
-
+    console.log(`Task Status: ${JSON.stringify(task.entity.status)}`);
     try {
         const taskData = {
             data: [
