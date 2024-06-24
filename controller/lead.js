ee422a24f8188f07a60af9ddca2533818b139317
praveen soni <ece.praveen2003@gmail.com>
2024-06-24 13:29:09 +0530

    Add Logger

--- controller/lead.js
+++ controller/lead.js
@@ -3,6 +3,7 @@ const { postLeadToZohoCRM, updateLeadToZohoCRM } = require("../utils/leadHelper.
 exports.postLeadToCRM = async (req, res) => {
     try {
         const newLead = req.body;
+        console.log(`New Lead Log ${newLead}`);
         await postLeadToZohoCRM(newLead);
         return res.status(200).send('Lead processed successfully');
     } catch (error) {
@@ -14,10 +15,11 @@ exports.postLeadToCRM = async (req, res) => {
 exports.updateLeadToCRM = async (req, res) => {
     try {
         const updatedLead = req.body;
+        console.log(`Lead Update Log ${updatedLead}`);
         await updateLeadToZohoCRM(updatedLead);
         return res.status(200).send('Lead Update successfully');
     } catch (error) {
-        console.log('Error processing webhook request:', error);
+        console.log('Error processing webhook request for Lead :', error);
         return res.status(500).send('Error processing webhook request');
     }
 }
