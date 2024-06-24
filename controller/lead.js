4ecbe6eacd2c178e269043b1554aee32dcb239c7
praveen soni <ece.praveen2003@gmail.com>
2024-06-24 16:00:36 +0530

    remove sms

--- controller/lead.js
+++ controller/lead.js
@@ -1,10 +1,7 @@
 const { json } = require("express");
-const twilio = require('twilio');
+
 const { postLeadToZohoCRM, updateLeadToZohoCRM } = require("../utils/leadHelper.js");
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = new twilio(accountSid, authToken);
 
 exports.postLeadToCRM = async (req, res) => {
     try {
@@ -18,26 +15,17 @@ exports.postLeadToCRM = async (req, res) => {
     }
 }
 
-const sendSMSErrorMessage = (errorMessage) => {
-    const message = `Error processing webhook request for Lead: ${errorMessage}`;
-    client.messages.create({
-        body: message,
-        from: '+18777804236',
-        to: '+918839782589' 
-    }).then(message => console.log(`SMS sent: ${message.sid}`))
-    .catch(error => console.log('Error sending SMS:', error));
-};
 
 exports.updateLeadToCRM = async (req, res) => {
     try {
         const updatedLead = req.body;
         console.log(`Lead Update Log ${JSON.stringify(updatedLead)}`);
         await updateLeadToZohoCRM(updatedLead);
-        sendSMSErrorMessage(`Lead Data ${updatedLead}`)
+    
         res.status(200).send('Lead Update successfully');
     } catch (error) {
         console.log('Error processing webhook request for Lead :', error);
-        sendSMSErrorMessage(error.message);
+    
         res.status(200).json({ message: 'Error processing Update webhook request', error: error.message });
     }
 }
