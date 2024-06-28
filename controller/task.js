ca0c14315f3dd28fa2ed94bef390319a632f9e55
Ameesha Tiwari <ameeshatiwari23@gmail.com>
2024-06-28 10:56:50 +0530

    kylas sheet intregreating google sheet

--- controller/task.js
+++ controller/task.js
@@ -1,24 +1,27 @@
 const { PostTaskzoho, updateTaskToZohoCRM } = require("../utils/taskHelper");
 
 exports.postTaskToCRM = async (req, res) => {
-    try {
-        const newTask = req.body;
-        await PostTaskzoho(newTask);
-        return res.status(200).send('Task processed successfully');
-    } catch (error) {
-        console.log('Error processing webhook request:', error);
-        return res.status(500).send('Error processing webhook request');
-    }
-}
+  //   let newTask;
+  try {
+    const newTask = req.body;
+    // console.log(`Task Try Data : ${JSON.stringify(newTask)}`);
+    await PostTaskzoho(newTask);
+    return res.status(200).send("Task processed successfully");
+  } catch (error) {
+    // console.log(`Task Catch Data : ${JSON.stringify(newTask)}`);
+    console.log("Error processing webhook request:", error);
+    return res.status(500).send("Error processing webhook request");
+  }
+};
 
 exports.updateTaskToCRM = async (req, res) => {
-    try {
-        console.log(`Update Task Log ${req.body}`);
-        const updatedTask = req.body;
-        await updateTaskToZohoCRM(updatedTask);
-        return res.status(200).send('Task Updated successfully');
-    } catch (error) {
-        console.log('Error processing webhook request:', error);
-        return res.status(500).send('Error processing webhook request');
-    }
-}
+  try {
+    console.log(`Update Task Log ${req.body}`);
+    const updatedTask = req.body;
+    await updateTaskToZohoCRM(updatedTask);
+    return res.status(200).send("Task Updated successfully");
+  } catch (error) {
+    console.log("Error processing webhook request:", error);
+    return res.status(500).send("Error processing webhook request");
+  }
+};
